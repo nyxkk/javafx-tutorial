adbaaf5318f7a20f58634af80f2acfc22e7e21aa
Nix Koh <nixkoh8@gmail.com>
2026-08-29 02:37:32 +0800

    Apply package standard to Duke entry point

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,14 @@
+package javafxtutorial;
+
+/**
+ * Provides the command-line entry point for the starter Duke application.
+ */
 public class Duke {
+    /**
+     * Prints the starter greeting.
+     *
+     * @param args command-line arguments, which are not used.
+     */
     public static void main(String[] args) {
         System.out.println("Hello!");
     }
